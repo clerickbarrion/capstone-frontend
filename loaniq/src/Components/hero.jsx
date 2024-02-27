282b01349ca5d0f59801bd00df71add426205d27
benjibennett12 <benjaminbennett0412@gmail.com>
2024-02-27 16:19:13 -0500

    Made Folders to Organize the Projects for the FrontEnd

---
+++ loaniq/src/Components/hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from 
+
+export default function hero() {
+  return (
+    <div className="hero-home">
+      <h3>Find a Loan with LoanIQ</h3>
+      <p>Find the best loan for you with LoanIQ</p>
+      <button>Get Started</button>
+      <img></img>
+    </div>
+  );
+}
