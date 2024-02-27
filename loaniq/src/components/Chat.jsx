7b923987fc4d97636beba02fcf8f902714ebd0d2
clerickbarrion <clerickbarrion@gmail.com>
2024-02-27 16:14:56 -0500

    added chat history

--- loaniq/src/components/Chat.jsx
+++ loaniq/src/components/Chat.jsx
@@ -5,20 +5,25 @@ export default function Chat() {
     async function enterMessage(event){
         if(event.key === 'Enter'){
             let message = event.target.value
-            event.target.value = ''
+            event.target.blur()
+            event.target.value = ''.trim()
             let messages = document.getElementById('messages')
+            const history = []
+            messages.childNodes.forEach(child => history.push(child.textContent))
             let newMessage = document.createElement('p')
             newMessage.textContent = `You: ${message}`
             messages.appendChild(newMessage)
+            messages.scrollTop += 1000
             const botMessage = await fetch('http://localhost:4000/api/botMessage', {
-                body: JSON.stringify({message}), 
+                body: JSON.stringify({message,history}), 
                 method: 'POST', 
                 headers: {'Content-Type': 'application/json'}
             }).then(response => response.json()).then(data => data)
             let botReply = document.createElement('p')
             botReply.textContent = `Kale: ${botMessage}`
             messages.appendChild(botReply)
-            event.target.blur()
+            console.log(messages)
+            messages.scrollTop += 1000
         }
     }
     function openChat(){
