79e584e977958e56953b4ff10c4ceadf80c3e1b2
clerickbarrion <clerickbarrion@gmail.com>
2024-03-07 10:22:35 -0500

    payments

--- loaniq/src/Pages/Payments.jsx
+++ loaniq/src/Pages/Payments.jsx
@@ -1,9 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import Modal from 'react-modal'
 
+function countdown(endDate) {
+    const today = new Date()
+    const time = endDate - today
+    const days = Math.floor(time / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((time % (1000 * 60)) / 1000)
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`
+}
 
 function Loan(props) {
-    const [modalIsOpen, setModalIsOpen] = React.useState(false)
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [endDate, setEndDate] = useState(new Date())
+    const [timeLeft, setTimeLeft] = useState(countdown(new Date(props.endDate)))
+    const minimum = (props.loanAmount / props.loanTerm)
+
+    useEffect(()=>{
+        setInterval(()=>setTimeLeft(countdown(new Date(props.endDate))),1000)
+    },)
+
     const openModal = () => {setModalIsOpen(true)}
     const closeModal = () => {setModalIsOpen(false)}
     function makePayment(e){
@@ -20,12 +37,12 @@ function Loan(props) {
         <div>Loan Amount: {props.loanAmount}</div>
         <div>Loan Term: {props.loanTerm} months</div>
         <div>Amount Paid: {props.amountPaid}</div>
-        <div>Next payment due: {props.nextPayment}</div>
+        <div>Next payment due: {timeLeft}</div>
         <button onClick={openModal}>Make Payment</button>
-        
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
             <h2>Make Payment</h2>
-            <div>Minimum Payment: $50</div>
+            <div>Minimum Payment:  ${minimum.toFixed(2)}</div>
+            <div>End date: {String(new Date(props.endDate))}</div>
             <form>
                 <input type="text" placeholder="Enter amount" />
                 <button onClick={makePayment}>Pay</button>
@@ -50,7 +67,7 @@ export default function Payments() {
   return (
     <div>
       {loans.length ? loans.map(loan => {
-        return <Loan loanID={loan.loanid} loanType={loan.loan_type} loanAmount={loan.loan_amount} loanTerm={loan.loan_term} amountPaid={loan.amount_paid} nextPayment={"30 Days"}/>
+        return <Loan loanID={loan.loanid} loanType={loan.loan_type} loanAmount={loan.loan_amount} loanTerm={loan.loan_term} amountPaid={loan.amount_paid} endDate={Number(new Date(loan.applyDate)) + (Number(loan.loan_term) * 2629800000)} nextPayment={"30 Days"}/>
       }) : <h1>No loans found</h1>}
       <style jsx>
         {`
