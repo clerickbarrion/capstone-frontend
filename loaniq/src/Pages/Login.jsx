52991248aa1458536e194bf91e05b094415df1d3
clerickbarrion <132791511+clerickbarrion@users.noreply.github.com>
2024-03-08 08:30:00 -0500

    Update Login.jsx

--- loaniq/src/Pages/Login.jsx
+++ loaniq/src/Pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/signin", {
+    const response = await fetch("https://loaniq-server.glitch.me/signin", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
