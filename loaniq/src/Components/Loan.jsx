a60461341273d3e9258df7514534f3aca86e7759
clerickbarrion <132791511+clerickbarrion@users.noreply.github.com>
2024-03-11 14:09:13 -0400

    Update Loan.jsx

--- loaniq/src/Components/Loan.jsx
+++ loaniq/src/Components/Loan.jsx
@@ -1,28 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 
 export default function Loan(props) {
   const [modalIsOpen, setModalIsOpen] = React.useState(false);
-    const [timeLeft, setTimeLeft] = useState(countdown(new Date(props.applyDate)))
-    const minimum = (props.loanAmount / props.loanTerm)
+  const [timeLeft, setTimeLeft] = useState(
+    countdown(new Date(props.applyDate))
+  );
+  const minimum = props.loanAmount / props.loanTerm;
 
-    useEffect(()=>{
-        setInterval(()=>setTimeLeft(countdown(new Date(props.applyDate)),1000))
-    },)
+  useEffect(() => {
+    setInterval(() => setTimeLeft(countdown(new Date(props.applyDate)), 1000));
+  });
 
   function countdown(applyDate) {
-    const today = Number(new Date())
-    let nextMonth = Number(new Date(applyDate)) + 2629800000
+    const today = Number(new Date());
+    let nextMonth = Number(new Date(applyDate)) + 2629800000;
     while (nextMonth < today) {
-      nextMonth += 2629800000
+      nextMonth += 2629800000;
     }
-    const time = nextMonth - today
-    const daysLeft = Math.floor((time % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24))
-    const hoursLeft = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    const minutesLeft = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
-    const secondsLeft = Math.floor((time % (1000 * 60)) / 1000)
-    return `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`
-}
+    const time = nextMonth - today;
+    const daysLeft = Math.floor(
+      (time % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+    );
+    const hoursLeft = Math.floor(
+      (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutesLeft = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((time % (1000 * 60)) / 1000);
+    return `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
+  }
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -31,7 +37,7 @@ export default function Loan(props) {
     setModalIsOpen(false);
   };
   function makePayment(e) {
-    fetch(`https://loaniq-server.glitch.me/makePayment`, {
+    fetch(`http://localhost:4000/makePayment`, {
       body: JSON.stringify({
         LoanID: props.loanID,
         AmountPaid: e.target.previousSibling.value,
@@ -43,40 +49,70 @@ export default function Loan(props) {
   }
   return (
     <div>
-      <div className="loan">
-        <h1>{props.loanType}</h1>
-        <div>Loan Amount: {props.loanAmount}</div>
-        <div>Loan Term: {props.loanTerm} months</div>
-        <div>Amount Paid: {props.amountPaid}</div>
-        <div>Next payment due: {timeLeft}</div>
-        <button onClick={openModal}>Make Payment</button>
-        <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-          <h2>Make Payment</h2>
-          <div>Minimum Payment: ${minimum.toFixed(2)}</div>
-          <form>
-            <input type="number" placeholder="Enter amount" />
-            <button onClick={makePayment}>Pay</button>
-          </form>
-          <button onClick={closeModal}>Close</button>
+      <div
+        className="loan card"
+        style={{ width: "18rem", margin: "10px", backgroundColor: "#e2d2b6" }}
+      >
+        <div className="card-header fs-4 text-center">
+          <h1>
+            <strong>{props.loanType}</strong>
+          </h1>
+        </div>
+        <div className="card-body">
+          <h5 className="card-text mb-2 fs-6">
+            Loan Amount: {props.loanAmount}
+          </h5>
+          <p className="card-text mb-2">Loan Term: {props.loanTerm} months</p>
+          <p className="card-text mb-2">Amount Paid: {props.amountPaid}</p>
+          <p className="card-text mb-2">Next payment due: {timeLeft}</p>
+          <div className="text-center mt-3">
+            <button onClick={openModal} className="btn btn-success text-light">
+              Make Payment
+            </button>
+          </div>
+        </div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={{
+            overlay: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+            content: {
+              width: "350px",
+              height: "350px",
+              margin: "auto",
+              border: "3px solid black",
+            },
+          }}
+        >
+          <div className="Modal-content  mt-5">
+            <h2 className="card-header mb-3 text-center">Make Payment</h2>
+            <div className="card-text mb-3 text-center">
+              Minimum Payment: ${minimum.toFixed(2)}
+            </div>
+            <form>
+              <input
+                className="form-control mb-2"
+                type="number"
+                placeholder="Enter amount"
+              />
+              <div className="text-center">
+                <button
+                  className="btn btn-success px-3 me-2"
+                  onClick={makePayment}
+                >
+                  Pay
+                </button>
+              </div>
+            </form>
+            <button
+              className="btn btn-danger position-absolute top-0 end-0 m-2"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
         </Modal>
       </div>
-      <style jsx>{`
-      .loan {
-        margin-top: 50px;
-        border: 1px solid #000;
-        padding: 20px;
-        background-color: #E2D2B6;
-        border-radius: 10px;
-        width: 300px;
-        margin: 10px;
-      }
-      .loan button {
-        border-radius: 10px;
-        padding: 5px;
-        background-color: #182D09;
-        color: white;
-      }
-      `}</style>
     </div>
   );
 }
